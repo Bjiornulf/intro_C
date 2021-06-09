7bf1344f9f0f8099f1bac241c58a70a8c62e0b1b
Bjiornulf <tpeev01@gmail.com>
2021-06-09 09:15:40 +0200

    simplification

--- 4_TD/list.c
+++ 4_TD/list.c
@@ -3,16 +3,6 @@
 
 List *push_list(void *data, List *list)
 {
-	if (list == NULL) {
-		List *new = (List *) malloc(sizeof(List));
-		new->data = data;
-		new->next = NULL;
-		return new;
-	}
-	if (list->data == NULL){
-		list->data = data;
-		return list;
-	}
 	List *new = (List *) malloc(sizeof(List));
 	new->next = list;
 	new->data = data;
