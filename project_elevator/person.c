03a05c59765ee52497c1c2601fb3adb0c5bb8a58
Bjiornulf <tpeev01@gmail.com>
2021-06-09 23:09:05 +0200

    person functionalities

---
+++ project_elevator/person.c
@@ -0,0 +1,41 @@
+#include <stdlib.h>
+#include "person.h"
+
+Person *createPerson(int src, int dest)
+{
+	Person *person = (Person *) malloc(sizeof(Person));
+	person->src = src;
+	person->dest = dest;
+
+	return person;
+}
+
+PersonList *insert(Person *p, PersonList *list)
+{
+	PersonList *head = (PersonList *) malloc(sizeof(PersonList));
+	head->person = p;
+	head->next = list;
+
+	return head;
+}
+
+int list_length(PersonList *list)
+{
+	int len = 0;
+	for (;list != NULL; list = list->next)
+		len++;
+	return len;
+}
+
+PersonList *reverse(PersonList *list)
+{
+	PersonList *new = NULL;
+	PersonList *temp;
+	while (list != NULL) {
+		temp = list->next;
+		list->next = new;
+		new = list;
+		list = temp;
+	}
+	return new;
+}
